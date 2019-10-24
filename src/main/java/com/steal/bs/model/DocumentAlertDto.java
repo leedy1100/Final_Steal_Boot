dc749877f7dfdf4874e935af77e261b0330d65e4
이태식 <dlxotlr99@naver.com>
2019-10-24 20:42:29 +0900

    문서

---
+++ src/main/java/com/steal/bs/model/DocumentAlertDto.java
@@ -0,0 +1,12 @@
+package com.steal.bs.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DocumentAlertDto {
+	private String title;
+	private String writer;
+	private String deadline;
+}
